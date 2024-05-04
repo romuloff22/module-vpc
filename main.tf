e050f7a3bbd4b4eb76f6fa306d445bc9d21c02d7
romuloff22 <romuloffranco@gmail.com>
2024-05-04 07:16:46 -0300

    Adicionando modulo de VPC AWS

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.47.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "remote-state-homolog"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  default_tags {
+    tags = {
+      owner       = "romulo"
+      managed-by  = "terraform"
+      environment = "Homolog"
+    }
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,63 @@
+resource "aws_vpc" "vpc-homo" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-homolog"
+  }
+}
+
+resource "aws_subnet" "subnet-homo1" {
+  vpc_id     = aws_vpc.vpc-homo.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet01-homolog"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc-homo.id
+
+  tags = {
+    Name = "internet-gateway-homolog"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc-homo.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "route-table-homolog"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet-homo1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+
+resource "aws_security_group" "security_group" {
+  name        = "seurit-group-homolog"
+  description = "Permitir acesso a porta 22"
+  vpc_id      = aws_vpc.vpc-homo.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
